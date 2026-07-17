df16cc629c83bcf7b96ebed009f290bd248f8334
Aymogul <aymogul22@gmail.com>
2026-07-17 01:00:50 +0100

    data and output

--- Day-05/main.tf
+++ Day-05/main.tf
@@ -3,3 +3,7 @@ resource "aws_s3_bucket" "demo" {
 
   tags = local.common_tags # Local variable (tags)
 }
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"    
+  region    = var.aws_region # Variable reference
+}
