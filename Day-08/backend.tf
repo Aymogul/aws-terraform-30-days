e68ddbc8a94dce980234e32bff459e568d3e389a
Aymogul <aymogul22@gmail.com>
2026-07-30 00:50:05 +0100

    backend.tf fix

--- Day-08/backend.tf
+++ Day-08/backend.tf
@@ -1,16 +1,8 @@
-#configure the AWS provider
+# create the backend for this project
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket = "ay-terraform-remote-backend-s3"
-    key    = "dev/terraform.tfstate"
+    key    = "day-08/terraform.tfstate"
     region = "us-east-1"
-    encrypt = true
-    use_lockfile = true
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}   
+}
