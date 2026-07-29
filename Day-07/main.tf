0c552918a12f6f058cb50f146a31346ad37b4a51
Aymogul <aymogul22@gmail.com>
2026-07-29 02:08:38 +0100

    final batch

--- Day-07/main.tf
+++ Day-07/main.tf
@@ -1,8 +1,9 @@
 # create ec2 instance
 resource "aws_instance" "my_ec2" {
-  ami             = "ami-0c55b159cbfafe1f0"
+  ami             = "ami-02b64aa047cb5edf5"
   count           = 1
   instance_type   = "t2.micro"
+  user_data      = file("user_data.sh")
   
   tags = {
     Name = "${var.project_name}-${var.environment}-ec2"
--- Day-07/vpc.tf
+++ Day-07/vpc.tf
@@ -85,6 +85,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 data "aws_availability_zones" "available" {
