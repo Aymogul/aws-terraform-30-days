6541dcacecf1619aae35430c45184a0788c2af13
Aymogul <aymogul22@gmail.com>
2026-07-30 00:37:32 +0100

    main.tf

--- Day-08/variables.tf
+++ Day-08/variables.tf
@@ -77,3 +77,4 @@ variable "network_config" {
   type        = tuple([string, string, number])
   description = "Network configuration (VPC CIDR, Subnet CIDR, subnet count)"
   default     = ["10.0.0.0/16", "10.0.1.0/24", 3]
+}
